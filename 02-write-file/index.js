e86d293fc771d3889204b9dfa68c52df10ed62ac
Anvar <fort2652@gmail.com>
2023-05-03 22:43:12 +0300

    feat: 02-write-file done

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const { stdin, stdout,stderr } = process;
+const readline = require('readline');
+const fs = require('fs')
+const path = require('path')
+
+stdout.write('Hello,write your text \n')
+
+stdin.on('data', data => {
+  fs.appendFile(path.join(__dirname,'/','text.txt'),data.toString(),err => {
+    if(err) throw err
+  })
+})
+
+process.on('SIGINT', () => {
+  stdout.write('Bye \n')
+  process.exit()
+})
