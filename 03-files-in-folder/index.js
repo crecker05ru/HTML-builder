306013012dd0dd33d55ca10ec8adc6dab2719c4b
Anvar <fort2652@gmail.com>
2023-05-06 16:07:21 +0300

    ref: refactor 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,41 +8,19 @@ targetFolder = path.join(__dirname,targetFolder)
 
 try {
   (async () => {
-    // const files = await readdir(path.dirname(__filename,'secret-folder'));
-    // const stat = await fs.stat(path.dirname(__filename,'secret-folder'))
-
-    const files = await readdir(targetFolder);
     let direns = await readdir(targetFolder,{withFileTypes: true});
-    console.log('diren',direns)
-    console.log('files',files)
-    // fs.stat(path.resolve(__filename),(err,stats) => {
-    //   console.log('stats',stats)
-    //   console.log('stats.isFile()',stats.isFile())
-    // })
+
     for(const diren of direns){
-      // console.log('diren',diren,'path.extname(diren)',path.extname(diren.name))
-      // console.log('diren.isDirectory()',diren.isDirectory())
       if(diren.isDirectory()){
         let folder = await readdir(path.join(targetFolder,diren.name))
       }else {
         let statistic = await stat(path.join(targetFolder,diren.name))
-        // console.log('__dirname',__dirname)
         let fileSizeKb = Number(statistic.size) === 0 ?  statistic.size : (statistic.size / 1000)
         console.log(diren.name.replace(path.extname(diren.name),''),'-',path.extname(diren.name).replace('.',''),'-',fileSizeKb + 'kb')
       }
     }
-    // for (const file of files) {
-    //   console.log('file',file,'path.extname(file)',path.extname(file))
-    //   // console.log('path.extname(file)',path.extname(file))
-    //   // console.log('stat',stat)
-    // };
   })()
 
 } catch (err) {
   console.error(err);
 }
-
-console.log('__filename',__filename)
-console.log('__dirname',__dirname)
-console.log('targetFolder',targetFolder)
-// console.log(path.extname(__filename,'./secret-folder'))
