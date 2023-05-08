3806fa38e73ca9d4f889a4417d91a1bcf38ead25
Anvar <fort2652@gmail.com>
2023-05-08 20:59:07 +0300

    fix: 02-write-file type exit end process

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,10 +6,15 @@ const path = require('path')
 stdout.write('Hello,write your text \n')
 
 stdin.on('data', data => {
+  if(data.toString().trim() === "exit"){
+    stdout.write('Bye \n')
+    process.exit()
+  }
   fs.appendFile(path.join(__dirname,'/','text.txt'),data.toString(),err => {
     if(err) throw err
   })
 })
+
 
 process.on('SIGINT', () => {
   stdout.write('Bye \n')
